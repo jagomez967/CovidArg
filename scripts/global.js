6f5c94021caf9d9f8c73816de45d04a08fb320a4
Alejandro <jagomez967@hotmail.com>
2020-04-25 18:25:31 -0300

    Separo en archivos

---
+++ scripts/global.js
@@ -0,0 +1,9 @@
+
+const URLAPI = {
+    Countries: "https://api.covid19api.com/countries"
+};
+const PAIS_DEFAULT = "argentina";
+const CASOS_LABEL = "Casos";
+const DECESOS_LABEL = "Decesos";
+const RECUPERADOS_LABEL = "Recuperados";
+ 
---
+++ scripts/utils.js
@@ -0,0 +1,46 @@
+async function getAsyncData(url) {
+  let fetchData = await fetch(url, {
+    method: "Get",
+    cache: "force-cache"
+  });
+  return fetchData.json();
+}
+
+Date.prototype.addHours = function (h) {
+  this.setTime(this.getTime() + h * 60 * 60 * 1000);
+  return this;
+};
+function dateToUtc(fecha) {
+  let date = new Date(fecha);
+  return new Date(date.getTime() + date.getTimezoneOffset() * 60000);
+}
+function isDateBetween(currentDate, minDate, maxDate) {
+  let dHasta = dateToUtc(maxDate);
+  let dDesde = dateToUtc(minDate);
+  let dCurr = dateToUtc(currentDate);
+
+  return dCurr >= dDesde && dHasta >= dCurr;
+}
+
+function sortAlphabetical(a, b) {
+  if (a > b) {
+    return -1;
+  }
+  if (b > a) {
+    return 1;
+  }
+  return 0;
+}
+
+function formatDate(fecha, separador) {
+  let d = dateToUtc(fecha);
+
+  let month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return [year, month, day].join(separador);
+}
